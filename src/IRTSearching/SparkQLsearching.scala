e57f91a343b8780361bf491302a42a70ec5215f2
JINGXUAN WEI <JINGXUAN WEI>
2016-09-08 15:45:24 +1000

    Initial commit

---
+++ src/IRTSearching/SparkQLsearching.scala
@@ -0,0 +1,20 @@
+package IRTSearching
+
+import java.io.File
+import java.util
+import java.util.Scanner
+
+import com.mongodb.spark.MongoSpark
+import com.mongodb.spark.config.WriteConfig
+import org.apache.spark.{SparkConf, SparkContext}
+import org.bson.Document
+import org.json.JSONObject
+
+import scala.collection.JavaConversions._
+/**
+  * Created by Tom on 8/09/2016.
+  */
+object SparkQL {
+
+
+}
